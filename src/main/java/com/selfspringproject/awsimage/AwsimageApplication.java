d15d96feab04470b19c495a73dd48a6efd452e8b
saurav012 <53165012+saurav012@users.noreply.github.com>
2021-01-28 13:07:48 +0530

    First commit

---
+++ src/main/java/com/selfspringproject/awsimage/AwsimageApplication.java
@@ -0,0 +1,13 @@
+package com.selfspringproject.awsimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsimageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsimageApplication.class, args);
+	}
+
+}
